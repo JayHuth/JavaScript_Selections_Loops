4c9bdbf52e075e2724466bd4ba7f641e70292c01
Justin Huth <Jayhuth96@gmail.com>
2023-03-22 10:10:24 -0500

    Completed

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+
+for (let i = 1; i <= 100; i+=2) {   
+        console.log(i);
+    }
+
+
 console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
+let x = 1;
+
+do {
+    let output = "";
+
+    if (x % 3 == 0) {
+        output += "FIZZ";
+    }
+
+    if (x % 5 == 0) {
+        output += "BUZZ";
+    }
+
+    console.log('${x} ${output}');
+
+    x++;
+} while (x <= 100);
+
 console.log("EXERCISE 2:\n==========\n");
